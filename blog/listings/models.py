8008fa52c86728f154255f83ca8d939d68e96f78
asbilim <bilimdepaullilian@gmail.com>
2022-08-21 00:43:31 +0100

    the users can add and see details of blogs

--- blog/listings/models.py
+++ blog/listings/models.py
@@ -6,11 +6,11 @@ class Post(models.Model):
 
     author = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
-    description = models.CharField(max_length=10000)  
+    description = models.TextField(default='none')
     date_created = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
 
 
     def __str__(self):
 
-        return self.title + '\n' + self.description
+        return self.title 
